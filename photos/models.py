828826e7cc66c81bbde990a028d7adbdaa96dd4e
Luis Barriga <Luis Barriga>
2017-03-15 22:48:42 +0100

    Modificación del modelo

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.auth.models import User
 from django.db import models
 
 COPYRIGHT = 'RIG'
@@ -10,14 +13,23 @@ LICENSES = (
     (CREATIVE_COMMONS, 'Creative Commons')
 )
 
+PUBLIC = 'PUB'
+PRIVATE = 'PRI'
+
+VISIBILITY = (
+    (PUBLIC, 'Publica'),
+    (PRIVATE, 'Privada')
+)
 
 class Photo(models.Model):
+    owner = models.ForeignKey(User)
     name = models.CharField(max_length=150)
     url = models.URLField()
     description = models.TextField(blank=True, null=True, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     license = models.CharField(max_length=3, choices=LICENSES)
+    visibility = models.CharField(max_length=3, choices=VISIBILITY, default=PUBLIC)
 
     def __str__(self):
         return self.name
